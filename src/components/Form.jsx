c4b43769f41fadb7520e937182da97abd2c2fd88
JoseReyesY <92343754+JoseReyesY@users.noreply.github.com>
2024-05-26 13:49:37 -0600

    Parte 11. Estilos del input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import styles from './form.module.css';
+
 export default function Form({ todos, setTodos }) {
     const [ todo, setTodo ] = useState("");
     function handleSubmit(e) {
@@ -12,9 +14,11 @@ export default function Form({ todos, setTodos }) {
     }
 
     return(
-        <form onSubmit={handleSubmit} >
-            <input type="text" onChange={ (e) => setTodo(e.target.value) } value={ todo } />
-            <button type="submit">Add</button>
+        <form className={ styles.todoForm } onSubmit={handleSubmit} >
+            <div className={ styles.inputContainer }>
+                <input className={ styles.modernInput } type="text" onChange={ (e) => setTodo(e.target.value) } value={ todo } placeholder="Enter todo item..." />
+                <button className={ styles.modernButton } type="submit">Add</button>
+            </div>
         </form>
     );
 }
