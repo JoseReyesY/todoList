d13730b6e048e088df24282552528704cb0c404a
JoseReyesY <92343754+JoseReyesY@users.noreply.github.com>
2024-06-20 15:42:00 -0600

    Parte 15. Contando los todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Form from "./Form";
 import TodoList from "./TodoList";
+import Footer from "./Footer";
 
 export default function Todo() {
     const [ todos, setTodos ] = useState([]);
+
+    const completedTodos = todos.filter((todo) => todo.done === true).length;
+    const totalTodos = todos.length;
 
     return(
         <div>
@@ -14,6 +18,10 @@ export default function Todo() {
             <TodoList 
                 todos = { todos }
                 setTodos = { setTodos }
+            />
+            <Footer
+                completedTodos = { completedTodos }
+                totalTodos = { totalTodos }
             />
         </div>
     );
