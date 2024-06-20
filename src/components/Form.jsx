99eb6ab3d1dc73f2ccbf3152f79c5bd15ae758a0
JoseReyesY <92343754+JoseReyesY@users.noreply.github.com>
2024-06-20 15:20:24 -0600

    Parte 14. Marcar como completado un todo

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,20 +3,22 @@ import { useState } from "react";
 import styles from './form.module.css';
 
 export default function Form({ todos, setTodos }) {
-    const [ todo, setTodo ] = useState("");
+    // const [ todo, setTodo ] = useState("");
+    const [ todo, setTodo ] = useState({ name: "", done: false });
+
     function handleSubmit(e) {
         e.preventDefault();
 
         // Add the todo to the list
         setTodos([...todos, todo]);
         // Reset the input
-        setTodo('');
+        setTodo({ name: "", done: false });
     }
 
     return(
         <form className={ styles.todoForm } onSubmit={handleSubmit} >
             <div className={ styles.inputContainer }>
-                <input className={ styles.modernInput } type="text" onChange={ (e) => setTodo(e.target.value) } value={ todo } placeholder="Enter todo item..." />
+                <input className={ styles.modernInput } type="text" onChange={ (e) => setTodo({name: e.target.value, done: false}) } value={ todo.name } placeholder="Enter todo item..." />
                 <button className={ styles.modernButton } type="submit">Add</button>
             </div>
         </form>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,25 @@
 import styles from './todoItem.module.css';
 
 export default function TodoItem({ item, todos, setTodos }) {
-    function handleDelete(item) {
-        console.log('Delete button clicked for item', item);
-        // It select all the todos which are not equal to the item
-        setTodos(todos.filter((todo) => todo !== item));
+    function handleCompleted(name) {
+        console.log('Item completed', name);
+        setTodos(todos.map((todo) => todo.name === name ? { ...todo, done: !todo.done } : todo));
     }
+
+    function handleDelete(name) {
+        console.log('Delete button clicked for item', name);
+        // It select all the todos which are not equal to the item
+        setTodos(todos.filter((todo) => todo.name !== name));
+    }
+
+    const completedClass = item.done ? styles.completed : "";
 
     return(
         <div className={ styles.item }>
             <div className={ styles.itemName }>
-                { item }
+                <span onClick={ () => handleCompleted(item.name) } className={ completedClass }>{ item.name }</span>
                 <span>
-                    <button onClick={ () => handleDelete(item) } className={ styles.deleteButton }>x</button>
+                    <button onClick={ () => handleDelete(item.name) } className={ styles.deleteButton }>x</button>
                 </span>
             </div>
             <hr className={ styles.line } />
